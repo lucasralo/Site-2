c4542c67e61894fcfbaa2fa7c8fcfbe04aaaad31
lucasralo <162320950+lucasralo@users.noreply.github.com>
2025-05-21 14:24:31 -0300

    21052025

---
+++ main.js
@@ -0,0 +1,14 @@
+const botoes = document.querySelectorAll(".botoes");
+const textos = document.querySelectorAll(".aba-conteudo");
+for (let i=0; i < botoes.length; i++){
+    botoes [i] .onclick = function(){
+        for (let j=0; j < botoes.length; j++){
+            botoes [j] .classList.remove("ativo");
+            textos [j] .classList.remove("ativo");
+        }
+        botoes [i].classList.add ("ativo");
+        textos [i].classList.add("ativo");
+    }
+}
+const contadores = document.querySelectorAll("contador");
+const tempoobjetivol = new Date ("2030-01-01T00:00:00")
